1dd21dbc3f16ac7934b642dea67494a398872f8c
Eliah Hooks <143127954+EliahHooks@users.noreply.github.com>
2025-07-02 18:23:05 -0500

    Updated the DDL/DML scripts to handle admin roles

--- MealPlannerDDLScript.sql
+++ MealPlannerDDLScript.sql
@@ -1,11 +1,13 @@
 DROP TABLE IF EXISTS Users;
--- Just sets up the User table for now
-CREATE TABLE Users (
+
+CREATE TABLE Users 
+(
     userID INT AUTO_INCREMENT,
     userName VARCHAR(20) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     height DOUBLE,
     weight DOUBLE,
     email VARCHAR(255) UNIQUE,
+    role ENUM('user', 'admin') DEFAULT 'user',
     PRIMARY KEY (userID)
 );
