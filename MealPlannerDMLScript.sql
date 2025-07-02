5338f3044d50c60713da96e76c8852c308863df5
Eliah Hooks <143127954+EliahHooks@users.noreply.github.com>
2025-07-02 18:29:53 -0500

    Updated the DML script with hashed passwords

--- MealPlannerDMLScript.sql
+++ MealPlannerDMLScript.sql
@@ -1,12 +1,24 @@
+-- Insert all fields into User (user1)
+INSERT INTO Users (userName, password, height, weight, email, role)
+VALUES (
+    'user1',
+    '$2y$10$2JDZh3fsG3yLidVsSg.YeOKgZsyqwEyHLcccpWZgVFL4V6D35llAq',
+    72, 150, 'johndoe@example.com', 'user'
+);
 
--- Insert all fields into User
-INSERT INTO Users (userName, password, height, weight, email)
-VALUES ('user1', 'password123', 72, 150, 'johndoe@example.com');
-
--- Inserting just username and password into User
-INSERT INTO Users (userName, password)
-VALUES ('user2', 'testPW123');
+-- Inserting just username and password into User (user2)
+INSERT INTO Users (userName, password, role)
+VALUES (
+    'user2',
+    '$2y$10$0YOTDFlV0fsYokCfbZOGVeq1TSUgdkKDq5KeMiMJRuTIv2xtXsTZ2',
+    'user'
+);
 
 -- Inserting an admin into the table
 INSERT INTO Users (userName, password, email, role)
-VALUES ('admin', 'admin', 'admin1@example.com', 'admin');
+VALUES (
+    'admin',
+    '$2y$10$EpOsHjfwViVszkRT1zd6Je3z0Zq5R6lE1YeGO.qZT0CN6b63WhK4i',
+    'admin1@example.com',
+    'admin'
+);
