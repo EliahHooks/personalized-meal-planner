09640a7a1fab70a58b8b2cd3ca776e22cfdbfe7f
Eliah Hooks <143127954+EliahHooks@users.noreply.github.com>
2025-07-02 18:24:19 -0500

    Updated the DDL/DML scripts to handle admin roles

--- MealPlannerDMLScript.sql
+++ MealPlannerDMLScript.sql
@@ -6,3 +6,7 @@ VALUES ('user1', 'password123', 72, 150, 'johndoe@example.com');
 -- Inserting just username and password into User
 INSERT INTO Users (userName, password)
 VALUES ('user2', 'testPW123');
+
+-- Inserting an admin into the table
+INSERT INTO Users (userName, password, email, role)
+VALUES ('admin', 'admin', 'admin1@example.com', 'admin');
