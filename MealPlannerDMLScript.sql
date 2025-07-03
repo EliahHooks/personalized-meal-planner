1027a3e045c260ce83a904526e4ce9eec2f4ace6
Eliah Hooks <143127954+EliahHooks@users.noreply.github.com>
2025-07-02 20:32:44 -0500

    updated DML script

--- MealPlannerDMLScript.sql
+++ MealPlannerDMLScript.sql
@@ -1,24 +1,27 @@
--- Insert all fields into User (user1)
+-- Clear existing data
+DELETE FROM Users;
+
+-- Insert user1 with password "password123"
 INSERT INTO Users (userName, password, height, weight, email, role)
 VALUES (
     'user1',
-    '$2y$10$2JDZh3fsG3yLidVsSg.YeOKgZsyqwEyHLcccpWZgVFL4V6D35llAq',
+    '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
     72, 150, 'johndoe@example.com', 'user'
 );
 
--- Inserting just username and password into User (user2)
+-- Insert user2 with password "password123"
 INSERT INTO Users (userName, password, role)
 VALUES (
     'user2',
-    '$2y$10$0YOTDFlV0fsYokCfbZOGVeq1TSUgdkKDq5KeMiMJRuTIv2xtXsTZ2',
+    '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
     'user'
 );
 
--- Inserting an admin into the table
+-- Insert admin with password "password123"
 INSERT INTO Users (userName, password, email, role)
 VALUES (
     'admin',
-    '$2y$10$EpOsHjfwViVszkRT1zd6Je3z0Zq5R6lE1YeGO.qZT0CN6b63WhK4i',
+    '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
     'admin1@example.com',
     'admin'
 );
